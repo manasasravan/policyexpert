9354df7623bc4bf5bf3f3bb205ecc5cee72803e9
manasasravan <manasasravan@gmail.com>
2022-02-19 16:48:07 +0000

    master: Added cucumber feature and stepdefs to open policyexprt home page

---
+++ src/test/java/com/policyexpert/home/stepdefs/HomeInsurance.java
@@ -0,0 +1,32 @@
+package com.policyexpert.home.stepdefs;
+
+import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.time.Duration;
+
+public class HomeInsurance {
+
+	private static WebDriver webDriver;
+
+	@BeforeAll
+	public static void initAll(){
+		String driverProperty = "webdriver.chrome.driver";
+		String driverLocation = System.getProperty(driverProperty);
+
+		if(driverLocation == null || driverLocation.isEmpty()){
+			System.setProperty(driverProperty, "src/test/resources/drivers/chromedriver.exe");
+		}
+		webDriver = new ChromeDriver();
+		webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		PageFactory.initElements(webDriver, new HomeInsurance());
+	}
+
+	@Given("I open the home insurance page {string}")
+	public void i_open_the_home_insurance_page(String website) {
+		webDriver.get(" https://insurance.policyexpert.co.uk/home");
+	}
+}
---
+++ src/test/resources/features/HomeInsurance.feature
@@ -0,0 +1,5 @@
+@all
+Feature: Policyexpert home insurance test page
+
+  Scenario:
+    Given I open the home insurance page "https://insurance.policyexpert.co.uk/home"
